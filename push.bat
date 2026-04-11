8c4f8391fe402828b15334ba1b584e5f7cde21b0
erik <ucok0502@gmail.com>
2026-04-11 12:40:14 +0700

    auto update

--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
-cd /d C:\mt4-sync
+cd /d C:\Users\DELL\AppData\Roaming\MetaQuotes\Terminal\51615F5B21EB6EEC08D70B45B8B59F73\MQL4\Files\mt4_sync
 
 "C:\Program Files\Git\cmd\git.exe" add .
 
@@ -9,3 +9,6 @@ IF %ERRORLEVEL% EQU 0 (
 
 "C:\Program Files\Git\cmd\git.exe" commit -m "auto update"
 "C:\Program Files\Git\cmd\git.exe" push
+
+
+cd /d C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\Common\Files\mt4-sync
